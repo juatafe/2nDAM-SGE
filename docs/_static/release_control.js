26462f7fba4cd686d0e84405c45de0d3861f4bf2
juatafe <juatafe@ieseljust.com>
2025-09-10 08:04:51 +0200

    ud1

--- docs/_static/release_control.js
+++ docs/_static/release_control.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const today = new Date();
-  document.querySelectorAll("details.dropdown").forEach(el => {
-    const release = el.getAttribute("data-release");
-    if (release) {
+  document.querySelectorAll("details.sd-dropdown").forEach(el => {
+    const span = el.querySelector(".release-date");
+    if (span) {
+      const release = span.dataset.release;
       const rDate = new Date(release);
       if (today < rDate) {
-        el.innerHTML = "<p><em>La solució estarà disponible a partir del "
-          + rDate.toLocaleDateString() + ".</em></p>";
+        el.innerHTML =
+          "<p><em>La solució estarà disponible a partir del " +
+          rDate.toLocaleDateString() +
+          ".</em></p>";
       }
     }
   });
